9f76fb6c6998afe4e032ac34c82a49b8351581a9
suisdavid <18217722446@163.com>
2026-08-06 15:17:49 +0800

    deleted legacy

--- committer.hpp
+++ committer.hpp
@@ -7,137 +7,175 @@
 #include "Predictor.hpp"
 #include <iostream>
 extern Memory memory;
-extern Register reg[],pc;
+extern Register reg[], pc;
 extern bool halt;
 extern Predictor predictor;
-const int maxk=10;
-class Committer{
-    private:
-        ROB robs[maxk],buffer;
-        int l,r,time;
-        int debug_cnt;
-        bool has_buffer;
-    public:
-        int total,right;//for branch predicting
-        Committer(){
-            l=0;r=0;time=0;
-            debug_cnt=0;
-            has_buffer=0;
-            total=right=0;
-        }
-        bool full(){
-            return (r+1)%maxk==l;
-        }
-        void add(ROB rob){
-            buffer=rob;has_buffer=1;
-        }
-        void update(){
-            if (has_buffer)
-            {
-                robs[r]=buffer;
-                r=(r+1)%maxk;
-                has_buffer=0;
-            }
-            if (time){
-                time--;
-                if (time==0){
-                    l=(l+1)%maxk;
-                }
-            }
-        }
-        pair<unsigned int,bool>read(int prod)
-        {
-            pair<unsigned,bool>ans;ans.second=0;
-            for (int id=l;id!=r;id=(id+1)%maxk){
-                if (robs[id].id==prod){
-                    if (robs[id].ready){
-                        ans.first=robs[id].value;
-                        ans.second=1;
-                    }
-                    break;
-                }
-            }
-            return ans;
-        }
+const int maxk = 10;
+class Committer
+{
+private:
+    ROB robs[maxk], buffer;
+    int l, r, time;
+    int debug_cnt;
+    bool has_buffer;
 
-        unsigned long long find(int id,int L,int R){
-            unsigned long long val[4]={0xFFFF,0xFFFF,0xFFFF,0xFFFF};
-            for (int i=l;i!=r;i=(i+1)%maxk)
-            {
-                if (robs[i].id>=id){break;}
-                if (robs[i].type>0&&robs[i].ready&&robs[i].dest<R&&robs[i].dest+robs[i].type>L){//ready store operations that overlap
-                    for (int j=0;j<robs[i].type;j++){
-                        int offset=robs[i].dest+j-L;
-                        if (offset>=0&&offset<R-L){
-                               val[offset]=((robs[i].value>>(8*j))&255);
-                        }
-                    }
-                }
-            }
-            return val[0]|(val[1]<<16)|(val[2]<<32)|(val[3]<<48);
-        }
-
-        pair<int,int> Commit()
+public:
+    int total, right; // for branch predicting
+    Committer()
+    {
+        l = 0;
+        r = 0;
+        time = 0;
+        debug_cnt = 0;
+        has_buffer = 0;
+        total = right = 0;
+    }
+    bool full()
+    {
+        return (r + 1) % maxk == l;
+    }
+    void add(ROB rob)
+    {
+        buffer = rob;
+        has_buffer = 1;
+    }
+    void update()
+    {
+        if (has_buffer)
         {
-            pair<int,int>ans=make_pair(0,0);
-            if (!time){
-                if (l!=r&&robs[l].ready){
-                    time=1;
-                    if (robs[l].type>0){//store memory
-                        time=2;
-                        unsigned int value=robs[l].value;
-                        for (int i=0;i<robs[l].type;i++){
-                            if (!memory.store(value&255,robs[l].dest+i)){//cache not hit
-                                time=3;
-                            }
-                            value>>=8;
-                        }
-                       // cout<<"memory["<<cur_rb.dest<<"] changed to "<<cur_rb.value<<"(length "<<cur_rb.type<<")"<<endl;
-                        //ans=make_pair(robs[l].id,0);
-                    }
-                    else if (robs[l].type==0){//alu
-                        if (robs[l].dest==(unsigned int)-1){//halt
-                            std::cout<<(reg[a0].val&255)<<std::endl;halt=true;
-                        }
-                        else{
-                            reg[robs[l].dest].val=robs[l].value;
-                        //  cout<<"reg["<<cur_rb.dest<<"] changed to "<<cur_rb.value<<endl;
-                            if (reg[robs[l].dest].prod==robs[l].id){
-                                reg[robs[l].dest].prod=-1;
-                            }
-                        }
-                    }
-                    else{//branch
-                        total++;
-                        if (robs[l].value&1){//needs to revert
-                            ans=make_pair(-robs[l].id,robs[l].dest);//need to clear all RS > id
-                        }
-                        else{
-                            right++;
-                        }
-                        predictor.add(-robs[l].type-1,robs[l].value);
-                    }
-                }
-            }
-            return ans;
+            robs[r] = buffer;
+            r = (r + 1) % maxk;
+            has_buffer = 0;
         }
-        void receive(CDB cdb){
-            for (int id=l;id!=r;id=(id+1)%maxk){
-                if (robs[id].id==cdb.id&&robs[id].ready==false){
-                    if (cdb.dest!=-2)// -2 is for branch operations, -1 is for halt
+        if (time)
+        {
+            time--;
+            if (time == 0)
+            {
+                l = (l + 1) % maxk;
+            }
+        }
+    }
+    pair<unsigned int, bool> read(int prod)
+    {
+        pair<unsigned, bool> ans;
+        ans.second = 0;
+        for (int id = l; id != r; id = (id + 1) % maxk)
+        {
+            if (robs[id].id == prod)
+            {
+                if (robs[id].ready)
+                {
+                    ans.first = robs[id].value;
+                    ans.second = 1;
+                }
+                break;
+            }
+        }
+        return ans;
+    }
+
+    unsigned long long find(int id, int L, int R)
+    {
+        unsigned long long val[4] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+        for (int i = l; i != r; i = (i + 1) % maxk)
+        {
+            if (robs[i].id >= id)
+            {
+                break;
+            }
+            if (robs[i].type > 0 && robs[i].ready && robs[i].dest < R && robs[i].dest + robs[i].type > L)
+            { // ready store operations that overlap
+                for (int j = 0; j < robs[i].type; j++)
+                {
+                    int offset = robs[i].dest + j - L;
+                    if (offset >= 0 && offset < R - L)
                     {
-                        robs[id].dest=cdb.dest;
-                        robs[id].value=cdb.val;
+                        val[offset] = ((robs[i].value >> (8 * j)) & 255);
+                    }
+                }
+            }
+        }
+        return val[0] | (val[1] << 16) | (val[2] << 32) | (val[3] << 48);
+    }
+
+    pair<int, int> Commit()
+    {
+        pair<int, int> ans = make_pair(0, 0);
+        if (!time)
+        {
+            if (l != r && robs[l].ready)
+            {
+                time = 1;
+                if (robs[l].type > 0)
+                { // store memory
+                    time = 2;
+                    unsigned int value = robs[l].value;
+                    for (int i = 0; i < robs[l].type; i++)
+                    {
+                        if (!memory.store(value & 255, robs[l].dest + i))
+                        { // cache not hit
+                            time = 3;
+                        }
+                        value >>= 8;
+                    }
+                    // cout<<"memory["<<cur_rb.dest<<"] changed to "<<cur_rb.value<<"(length "<<cur_rb.type<<")"<<endl;
+                }
+                else if (robs[l].type == 0)
+                { // alu
+                    if (robs[l].dest == (unsigned int)-1)
+                    { // halt
+                        std::cout << (reg[a0].val & 255) << std::endl;
+                        halt = true;
                     }
                     else
                     {
-                        robs[id].value=robs[id].value*2+cdb.val;
+                        reg[robs[l].dest].val = robs[l].value;
+                        //  cout<<"reg["<<cur_rb.dest<<"] changed to "<<cur_rb.value<<endl;
+                        if (reg[robs[l].dest].prod == robs[l].id)
+                        {
+                            reg[robs[l].dest].prod = -1;
+                        }
                     }
-                    robs[id].ready=true;
+                }
+                else
+                { // branch
+                    total++;
+                    if (robs[l].value & 1)
+                    {                                               // needs to revert
+                        ans = make_pair(-robs[l].id, robs[l].dest); // need to clear all RS > id
+                    }
+                    else
+                    {
+                        right++;
+                    }
+                    predictor.add(-robs[l].type - 1, robs[l].value);
                 }
             }
         }
-        void clear(){
-            l=r=time=0;
+        return ans;
+    }
+    void receive(CDB cdb)
+    {
+        for (int id = l; id != r; id = (id + 1) % maxk)
+        {
+            if (robs[id].id == cdb.id && robs[id].ready == false)
+            {
+                if (cdb.dest != -2) // -2 is for branch operations, -1 is for halt
+                {
+                    robs[id].dest = cdb.dest;
+                    robs[id].value = cdb.val;
+                }
+                else
+                {
+                    robs[id].value = robs[id].value * 2 + cdb.val;
+                }
+                robs[id].ready = true;
+            }
         }
+    }
+    void clear()
+    {
+        l = r = time = 0;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -12,79 +12,100 @@
 using namespace std;
 
 Memory memory;
-Register reg[32],pc;//record the last unknown store
+Register reg[32], pc; 
 Decoder decoder;
 Issuer issuer;
 ALU alu;
 LSU lsu;
 BRU bru;
-CDB alu_cdb,lsu_cdb,bru_cdb,pre_alu_cdb,pre_lsu_cdb,pre_bru_cdb;
+CDB alu_cdb, lsu_cdb, bru_cdb, pre_alu_cdb, pre_lsu_cdb, pre_bru_cdb;
 Committer committer;
 Predictor predictor;
 bool halt;
-int flush_val,newaddr;//for flushing
+int flush_val, newaddr; // for flushing
 
-void broadcast(CDB cdb){
-    if (cdb.id==-1){return;}
+void broadcast(CDB cdb)
+{
+    if (cdb.id == -1)
+    {
+        return;
+    }
     alu.receive(cdb);
     lsu.receive(cdb);
     bru.receive(cdb);
     committer.receive(cdb);
 }
 
-
-int main(){
+int main()
+{
     memory.init();
-    int clk=0;
-    while (!halt){
-        clk++;reg[x0].val=0;reg[x0].prod=-1;//period initialize
-        if (flush_val){//flushing
-            issuer.nex_want=pc.val=newaddr;
-            alu.clear(flush_val);//clear all RS whose id>-res
+    int clk = 0;
+    while (!halt)
+    {
+        clk++;
+        reg[x0].val = 0;
+        reg[x0].prod = -1; // period initialize
+        if (flush_val)
+        { // flushing
+            issuer.nex_want = pc.val = newaddr;
+            alu.clear(flush_val); // clear all RS whose id>-res
             lsu.clear(flush_val);
             bru.clear(flush_val);
-            for (int i=0;i<32;i++){
-                if (reg[i].prod>flush_val){
-                    reg[i].prod=-1;
+            for (int i = 0; i < 32; i++)
+            {
+                if (reg[i].prod > flush_val)
+                {
+                    reg[i].prod = -1;
                 }
             }
             committer.clear();
-            decoder.id=-1;
-            issuer.id=-1;
-            flush_val=0;
+            decoder.id = -1;
+            issuer.id = -1;
+            flush_val = 0;
             continue;
         }
-        //fetch
-        pair<int,unsigned int>fetcher_output=make_pair(pc.val,memory.fetch(pc.val));
-        //decode
-        pair<int,op>decode_output=decoder.decode();
-        //issue
-        int new_pc_val=issuer.issue(clk);
-        //execute
-        CDB alu_cdb=alu.execute();
-        CDB lsu_cdb=lsu.execute();
-        CDB bru_cdb=bru.execute();
-        //commit
-        pair<int,int>committer_output=committer.Commit();
-        int res=committer_output.first;
-        if (res<0){//revert
-            flush_val=-res;
-            newaddr=committer_output.second;
+        // fetch
+        pair<int, unsigned int> fetcher_output = make_pair(pc.val, memory.fetch(pc.val));
+        // decode
+        pair<int, op> decode_output = decoder.decode();
+        // issue
+        int new_pc_val = issuer.issue(clk);
+        // execute
+        CDB alu_cdb = alu.execute();
+        CDB lsu_cdb = lsu.execute();
+        CDB bru_cdb = bru.execute();
+        // commit
+        pair<int, int> committer_output = committer.Commit();
+        int res = committer_output.first;
+        if (res < 0)
+        { // revert
+            flush_val = -res;
+            newaddr = committer_output.second;
         }
-        //update
-        decoder.id=fetcher_output.first;decoder.opcode=fetcher_output.second;
-        issuer.id=decode_output.first;issuer.cur_op=decode_output.second;
-        pc.val=new_pc_val;
-        broadcast(alu_cdb);broadcast(lsu_cdb);broadcast(bru_cdb);
-        broadcast(pre_alu_cdb);broadcast(pre_lsu_cdb);broadcast(pre_bru_cdb);
-        pre_alu_cdb=alu_cdb;pre_lsu_cdb=lsu_cdb;pre_bru_cdb=bru_cdb;
-        alu.update();lsu.update();bru.update();
+        // update
+        decoder.id = fetcher_output.first;
+        decoder.opcode = fetcher_output.second;
+        issuer.id = decode_output.first;
+        issuer.cur_op = decode_output.second;
+        pc.val = new_pc_val;
+        broadcast(alu_cdb);
+        broadcast(lsu_cdb);
+        broadcast(bru_cdb);
+        broadcast(pre_alu_cdb);
+        broadcast(pre_lsu_cdb);
+        broadcast(pre_bru_cdb);
+        pre_alu_cdb = alu_cdb;
+        pre_lsu_cdb = lsu_cdb;
+        pre_bru_cdb = bru_cdb;
+        alu.update();
+        lsu.update();
+        bru.update();
         committer.update();
         predictor.update();
         memory.update();
     }
-   // cout<<"clock="<<clk<<endl;
-   // cout<<"hit= "<<memory.hit<<" not hit= "<<memory.nothit<<" accuracy= "<<(double)memory.hit/(memory.hit+memory.nothit)<<endl;
-   // cout<<"branch predictor accuracy="<<(double)committer.right/committer.total<<endl;
+    // cout<<"clock="<<clk<<endl;
+    // cout<<"hit= "<<memory.hit<<" not hit= "<<memory.nothit<<" accuracy= "<<(double)memory.hit/(memory.hit+memory.nothit)<<endl;
+    // cout<<"branch predictor accuracy="<<(double)committer.right/committer.total<<endl;
     return 0;
 }
